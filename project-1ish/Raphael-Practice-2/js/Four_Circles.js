c9c960bfde478d9fb67fa4691f2997b7afd7a084
cmealo17 <christiemealo@Christies-MacBook-Pro.local>
2014-09-23 18:24:30 -0400

    interactive-2
    assignment 1

---
+++ project-1ish/Raphael-Practice-2/js/Four_Circles.js
@@ -0,0 +1,13 @@
+// converted from svg via http://www.readysetraphael.com/
+
+var rsr = Raphael('rsr', '800', '600'); 
+
+var PinkCircle = rsr.set(); var circle_a = rsr.circle(148, 141, 86).attr({class: 'st0',parent: 'PinkCircle','stroke-width': '0','stroke-opacity': '1','fill': '#000000'}).data('id', 'circle_a'); PinkCircle.attr({'id': 'PinkCircle','name': 'PinkCircle'}); 
+
+var OrangeCircle = rsr.set(); var circle_b = rsr.circle(291, 345, 57).attr({class: 'st1',parent: 'OrangeCircle','stroke-width': '0','stroke-opacity': '1','fill': '#000000'}).data('id', 'circle_b'); OrangeCircle.attr({'id': 'OrangeCircle','name': 'OrangeCircle'}); 
+
+var YellowCircle = rsr.set(); var circle_c = rsr.circle(501, 201, 97).attr({class: 'st2',parent: 'YellowCircle','stroke-width': '0','stroke-opacity': '1','fill': '#000000'}).data('id', 'circle_c'); YellowCircle.attr({'id': 'YellowCircle','name': 'YellowCircle'}); 
+
+var GreenCircle = rsr.set(); var circle_d = rsr.circle(644, 451, 84).attr({class: 'st3',parent: 'GreenCircle','stroke-width': '0','stroke-opacity': '1','fill': '#000000'}).data('id', 'circle_d'); GreenCircle.attr({'id': 'GreenCircle','name': 'GreenCircle'}); 
+
+var rsrGroups = [PinkCircle,OrangeCircle,YellowCircle,GreenCircle]; PinkCircle.push( circle_a ); OrangeCircle.push( circle_b ); YellowCircle.push( circle_c ); GreenCircle.push( circle_d );
